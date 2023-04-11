4bd701e16f3b69649cf971c5952be2a0b572d155
dragonkingl6 <maitienducik5@gmail,com>
2023-04-11 15:49:26 +0700

    create new project

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secret': 'huy tdb ff ',
+    'database': 'mongodb+srv://ducmong6969:wnQP25uZS5J8gHRp@maitienduc.gimicfu.mongodb.net/assignment?retryWrites=true&w=majority'
+  };
